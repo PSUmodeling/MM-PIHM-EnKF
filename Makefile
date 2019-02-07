17bb9a51becec5a5269fc55faf2d1e7006657dec
Yuning Shi <shiyuning@gmail.com>
2019-02-06 20:13:07 -0500

    :sparkles: Check highest MM-PIHM version that is compatible with EnKF system

--- Makefile
+++ Makefile
@@ -7,7 +7,8 @@ VERS = 0.1.0-alpha
 PIHM_DIR = $$HOME/work/MM-PIHM
 
 # Required MM-PIHM version
-RQD_PIHM_VERS = 0.9.0-alpha
+LOW_PIHM_VERS = 0.9.0-alpha
+HIGH_PIHM_VERS = 0.10.0-alpha
 
 CC = gcc
 CFLAGS = -g -O2
@@ -109,7 +110,7 @@ all: check_version pihmlib enkflib perturb assim
 	@chmod 755 ./util/*.sh
 
 check_version:
-	@util/check_version.sh $(PIHM_DIR) $(RQD_PIHM_VERS) $(VERS)
+	@util/check_version.sh $(PIHM_DIR) $(LOW_PIHM_VERS) $(HIGH_PIHM_VERS) $(VERS)
 
 pihmlib:
 	@mkdir -p lib
--- util/check_version.sh
+++ util/check_version.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 PIHM_DIR=$1
-REQ_VERSION=$2
-ENKF_VERSION=$3
+LOW_VERSION=$2
+HIGH_VERSION=$3
+ENKF_VERSION=$4
 
 # Check MM-PIHM version
 cd $PIHM_DIR
 CUR_VERS=$(./flux-pihm -V |grep "MM-PIHM" |awk '{print $3}')
 
 # Check if MM-PIHM version is compatible
-if [ "$(printf '%s\n' $CUR_VERS $REQ_VERSION | sort -V | head -n 1)" != "$REQ_VERSION" ]; then
-    echo "MM-PIHM EnKF v$ENKF_VERSION requires MM-PIHM v$REQ_VERSION or above."
+if [ "$(printf '%s\n' $CUR_VERS $LOW_VERSION $HIGH_VERSION | sort -V | sed -n 2p)" != "$CUR_VERS" ]; then
+    echo "MM-PIHM EnKF v$ENKF_VERSION requires MM-PIHM v$LOW_VERSION to v$HIGH_VERSION."
     echo "Currently MM-PIHM v$CUR_VERS is installed."
     echo "Please go to https://github.com/PSUmodeling/MM-PIHM/releases and choose a compatible version."
     exit 1

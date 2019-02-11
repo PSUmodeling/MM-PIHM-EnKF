b6ea5468a7a131e37b81a903e5bdf95f386d22b2
Yuning Shi <shiyuning@gmail.com>
2019-02-11 09:56:38 -0500

    :white_check_mark: Check if MM-PIHM directory and executable exist

--- util/check_version.sh
+++ util/check_version.sh
@@ -5,8 +5,23 @@ LOW_VERSION=$2
 HIGH_VERSION=$3
 ENKF_VERSION=$4
 
-# Check MM-PIHM version
+# Check if PIHM_DIR exists
+if [ ! -d $PIHM_DIR ]; then
+    echo "The MM-PIHM directory does not exist."
+    echo "Please check if the PIHM_DIR path in the Makefile is correct."
+    exit 1
+fi
+
 cd $PIHM_DIR
+
+# Check if Flux-PIHM executable exists
+if [ ! -e flux-pihm ]; then
+    echo "Cannot find Flux-PIHM executable in the MM-PIHM directory."
+    echo "Please check if the PIHM_DIR path in the Makefile is correct."
+    exit 1
+fi
+
+# Check MM-PIHM version
 CUR_VERS=$(./flux-pihm -V |grep "MM-PIHM" |awk '{print $3}')
 
 # Check if MM-PIHM version is compatible

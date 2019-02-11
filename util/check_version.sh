829a4b8dbe1f8e656714b26110ee996b969524bf
Yuning Shi <shiyuning@gmail.com>
2019-02-11 09:42:23 -0500

    :speech_balloon: Change text output

--- util/check_version.sh
+++ util/check_version.sh
@@ -11,7 +11,11 @@ CUR_VERS=$(./flux-pihm -V |grep "MM-PIHM" |awk '{print $3}')
 
 # Check if MM-PIHM version is compatible
 if [ "$(printf '%s\n' $CUR_VERS $LOW_VERSION $HIGH_VERSION | sort -V | sed -n 2p)" != "$CUR_VERS" ]; then
-    echo "MM-PIHM EnKF v$ENKF_VERSION requires MM-PIHM v$LOW_VERSION to v$HIGH_VERSION."
+    if [ "$HIGH_VERSION" = "above" ]; then
+        echo "MM-PIHM EnKF v$ENKF_VERSION requires MM-PIHM v$LOW_VERSION or $HIGH_VERSION."
+    else
+        echo "MM-PIHM EnKF v$ENKF_VERSION requires MM-PIHM v$LOW_VERSION to v$HIGH_VERSION."
+    fi
     echo "Currently MM-PIHM v$CUR_VERS is installed."
     echo "Please go to https://github.com/PSUmodeling/MM-PIHM/releases and choose a compatible version."
     exit 1
